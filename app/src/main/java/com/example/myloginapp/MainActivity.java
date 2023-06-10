7f6ce48a68eff549b8b91ef63e7885ca546aeb2f
om <kumarom95700@gmail.com>
2023-06-10 13:05:16 +0530

    hi

--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -3,19 +3,57 @@ package com.example.myloginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Xml;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
 import com.google.android.material.button.MaterialButton;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView forgotpassword = findViewById(R.id.forgotpass);
+        forgotpassword.setBackgroundResource(
+                R.drawable.label_bg
+        );
+        forgotpassword.setOnClickListener(new View.OnClickListener() {
+
+
+                    @Override
+                    public void onClick(View v) {
+
+                            Intent i = new Intent(MainActivity.this, ForgotPassword.class);
+                            startActivity(i);
+
+
+                    }
+                }
+
+
+
+
+
+
+
+
+        );
+
+
+        // creating clickable span to be implemented as a link
+
+
 
 
         TextView username =(TextView) findViewById(R.id.username);
@@ -45,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         adminbtn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v){
-            Intent i = new Intent(MainActivity.this, AdminPage.class);
+            Intent i = new Intent(MainActivity.this, AdmineLogin.class);
             startActivity(i);
 
         }
